acb6ca701bd332c99356fc689417a31333d8564a
ahaampo5 <ahaampo5@gmail.com>
2023-07-20 10:26:15 +0000

    [Update] ssh setup

---
+++ ssh_setup.sh
@@ -0,0 +1,12 @@
+Port=$1
+Autorized_keys=$2
+
+apt update
+apt install ssh
+sed -i 's/#Port 22/Port $Port/g' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+
+mkdir -p ~/.ssh
+echo $2 > ~/.ssh/authorized_keys
+
+service ssh start
