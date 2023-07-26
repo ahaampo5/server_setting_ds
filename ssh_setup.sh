d5bb452ae375e2bfa4f699e8ea7cfd86f8bafbc5
ahaampo5 <ahaampo5@gmail.com>
2023-07-26 07:05:30 +0000

    [Update] ssh setting stable

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -3,8 +3,8 @@ Autorized_keys=$2
 
 apt update
 apt install ssh
-sed -i 's/#Port */Port {$Port}/g' /etc/ssh/sshd_config
-sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+sed -i "s/#Port 22/Port ${Port}/g" /etc/ssh/sshd_config
+sed -i "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
 
 mkdir -p ~/.ssh
 echo $2 > ~/.ssh/authorized_keys
